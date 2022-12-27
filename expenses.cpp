f827421f16ffc897835446a2e1328fe0ee8079bc
Nikolay318 <121396504+Nikolay318@users.noreply.github.com>
2022-12-27 19:02:51 +0300

    Сделана реализация класса Expense

--- expenses.cpp
+++ expenses.cpp
@@ -1,1 +1,21 @@
 #include "expenses.h"
+
+Expense::~Expense() {
+    delete date;
+}
+
+Date* Expense::getDate(){
+    return date;
+}
+
+string Expense::getCategory(){
+    return category;
+}
+
+string Expense::getPayee(){
+    return payee;
+}
+
+float Expense::getAmount(){
+    return amount;
+}

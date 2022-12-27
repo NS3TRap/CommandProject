d7ebafe662357c2136be009f02770f56f1cadc49
ezhik89 <121419820+ezhik89@users.noreply.github.com>
2022-12-27 18:52:09 +0300

    Сделана реализация класса OrderInteractionScreen

--- orders.cpp
+++ orders.cpp
@@ -95,3 +95,61 @@ void ListOfOrders::display(){
         }
     }
 }
+void OrderInteractionScreen::addNewOrder(){
+    fio = new Fio;
+    system("cls");
+    cout << "Enter customer name:\n";
+    cin >> fio->fname;
+    cout << "Enter client's last name:\n";
+    cin >> fio->lname;
+    cout << "Enter customer patronymic:\n";
+    cin >> fio->sname;
+    cin.ignore(80, '\n');
+    if(ptrListOfClients->findClientByFio(fio)){
+        system("cls");
+        cout << "Enter the name of the task:\n";
+        getline(cin, taskName);
+        cout << "Enter the make of the car:\n";
+        getline(cin, car);
+        cout << "Enter price:\n";
+        cin >> cost;
+        cout << "Enter order status (in progress, completed):\n";
+        cin.ignore(80, '\n');
+        getline(cin, statusName);
+        Order* ptrOrder = new Order(taskName,fio,car,cost,statusName);
+        ptrListOfOrders->addNewOrders(ptrOrder);
+    }else {
+        cout << "No such client found!\n";
+        system("pause");
+    }
+
+}
+
+void OrderInteractionScreen::changeOrder(){
+    fio = new Fio;
+    string newStatus;
+    Order* ptrOrder;
+    system("cls");
+    cout << "Enter customer name:\n";
+    cin >> fio->fname;
+    cout << "Enter client's last name:\n";
+    cin >> fio->lname;
+    cout << "Enter customer patronymic:\n";
+    cin >> fio->sname;
+    cout << "Enter the name of the task:\n";
+    cin >> taskName;
+    cout << "Enter the make of the car:\n";
+    cin >> car;
+    cout << "Enter price:\n";
+    cin >> cost;
+    ptrOrder = ptrListOfOrders->findOrders(taskName,fio,car,cost);
+    if(ptrOrder != NULL){
+        cout << "Enter a new order status(in progress, completed):\n";
+        cin >> newStatus;
+        ptrOrder->changeStatusName(newStatus);
+    }else{
+        cout << "No such order found!\n";
+        system("pause");
+    }
+    delete fio;
+}

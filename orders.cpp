2aaf22159b222687043aecf9e30a6d09f940ab6b
ezhik89 <121419820+ezhik89@users.noreply.github.com>
2022-12-27 18:50:39 +0300

    Сделана реализация класса Order

--- orders.cpp
+++ orders.cpp
@@ -1,2 +1,27 @@
 #include "orders.h"
+#include <iostream>
 
+using namespace std;
+
+string Order::getTaskName(){
+    return taskName;
+}
+Fio* Order::getFio(){
+    return fio;
+}
+
+string Order::getCar(){
+    return car;
+}
+
+float Order::getCost(){
+    return cost;
+}
+
+string Order::getStatusName(){
+    return statusName;
+}
+
+void Order::changeStatusName(string newStatus){
+    statusName = newStatus;
+}

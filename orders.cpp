c326c66992e2aa452f5cacdc53473bdb6ef47d30
ezhik89 <121419820+ezhik89@users.noreply.github.com>
2022-12-27 18:51:27 +0300

    Сделана реализация класса ListOfOrders

--- orders.cpp
+++ orders.cpp
@@ -25,3 +25,73 @@ string Order::getStatusName(){
 void Order::changeStatusName(string newStatus){
     statusName = newStatus;
 }
+ListOfOrders::~ListOfOrders(){
+    while (!ptrOrdersList.empty())
+    {
+        iter = ptrOrdersList.begin();
+        delete *iter;
+        ptrOrdersList.erase(iter);
+    }
+}
+
+void ListOfOrders::addNewOrders(Order* ptrOrder){
+    ptrOrdersList.push_back(ptrOrder);
+}
+
+void ListOfOrders::changeStatusName(Order* ptrOrder, string newStatus){
+    ptrOrder->changeStatusName(newStatus);
+}
+
+float ListOfOrders::getSumOrders(){
+    float sum = 0;
+    iter = ptrOrdersList.begin();
+    while (iter != ptrOrdersList.end())
+    {
+        if ((*iter)->getStatusName() == "completed")
+        {
+            sum += (*iter)->getCost();
+        }
+        iter++;
+    }
+    return sum;
+}
+
+Order* ListOfOrders::findOrders(string taskName, Fio* fio, string car, float cost){
+    Fio* findFio;
+    iter = ptrOrdersList.begin();
+    while (iter != ptrOrdersList.end())
+    {
+    findFio = (*iter)->getFio();
+    if (findFio->fname == fio->fname && findFio->sname == fio->sname && findFio->lname == fio->lname &&
+        (*iter)->getTaskName() == taskName && (*iter)->getCar() == car && (*iter)->getCost() == cost)
+    {
+        delete fio;
+        return (*iter);
+    }
+    iter++;
+    }
+    return NULL;
+}
+
+void ListOfOrders::display(){
+    system("cls");
+    cout << "\nTask name  \t||Fio   \t\t\t||Cost   \t||Status name\n"<<
+            "--------------------------------------------------------------------------------------\n";
+    if (ptrOrdersList.empty())
+        cout << "***The list is empty***\n" << endl;
+    else
+    {
+        iter = ptrOrdersList.begin();
+        while (iter != ptrOrdersList.end())
+        {
+            cout << (*iter)->getTaskName() << "\t" << "||" <<
+                    (*iter)->getFio()->fname << " "<<
+                    (*iter)->getFio()->lname << " "<<
+                    (*iter)->getFio()->sname << "\t" << "||" <<
+                    (*iter)->getCar() << "\t" << "||" <<
+                    (*iter)->getCost() << "\t" << "||" <<
+                    (*iter)->getStatusName() << endl;
+            *iter++;
+        }
+    }
+}

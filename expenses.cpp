f45f993df3887515c737886fe0102c2f3f9bdc5a
Nikolay318 <121396504+Nikolay318@users.noreply.github.com>
2022-12-27 19:06:23 +0300

    Сделана реализация метода getExpensesRecord класса ExpensesRecord

--- expenses.cpp
+++ expenses.cpp
@@ -33,3 +33,25 @@ ExpensesRecord::~ExpensesRecord() {
 void ExpensesRecord::insertExp(Expense* ptrExpense){
     vectPtrsExpenses.push_back(ptrExpense);
 }
+
+void ExpensesRecord::getExpensesRecord(){
+    Date* date;
+    system("cls");
+    cout << "\nDate\tAddressee\tAmount\tCategory\n"
+    << "----------------------------------------\n" << endl;
+    if (vectPtrsExpenses.size() == 0) // В контейнере нет расходов
+        cout << "***Расходов нет***\n" << endl;
+    else
+    {
+        iter = vectPtrsExpenses.begin();
+    while (iter != vectPtrsExpenses.end())
+    { // распечатываем все расходы
+         date = (*iter)->getDate();
+         cout << date->month << '/' << date->day << '/'<<date->year<<
+         '\t' << (*iter)->getPayee() << '\t' << '\t';
+         cout << (*iter)->getAmount() << '\t' << (*iter)->getCategory() << endl;
+         iter++;
+    }
+    cout << endl;
+    }
+}
